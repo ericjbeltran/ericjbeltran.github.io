d02c3eea7d9dd2800176d44fc5ca07f24b24c8d6
Eric Beltran <ericjbeltran@gmail.com>
2016-11-02 10:28:34 -0400

    removed server script

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -24,22 +24,4 @@ $headers .= "Reply-To: $email_address";
 mail($to,$email_subject,$email_body,$headers);
 return true;
 
-server {
-        listen       80;
-        server_name  localhost;
-
-        location / {
-            root   html;
-            index  index.html index.htm;
-        }
-
-        error_page  404     /404.html;
-        error_page  403     /403.html;
-
-        # To allow POST on static pages
-        error_page  405     =200 $uri;
-        
-        # ...
-}
-
 ?>
