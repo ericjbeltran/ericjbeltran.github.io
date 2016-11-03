5d73c0264e6b008c170bc2c7932002290dd0d3b2
Eric Beltran <ericjbeltran@gmail.com>
2016-11-03 14:43:18 -0400

    removed /vendor/

--- gulpfile.js
+++ gulpfile.js
@@ -54,10 +54,10 @@ gulp.task('minify-js', function() {
 // Copy vendor libraries from /node_modules into /vendor
 gulp.task('copy', function() {
     gulp.src(['node_modules/bootstrap/dist/**/*', '!**/npm.js', '!**/bootstrap-theme.*', '!**/*.map'])
-        .pipe(gulp.dest('vendor/bootstrap'))
+        .pipe(gulp.dest('bootstrap'))
 
     gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/jquery/dist/jquery.min.js'])
-        .pipe(gulp.dest('vendor/jquery'))
+        .pipe(gulp.dest('jquery'))
 
     gulp.src([
             'node_modules/font-awesome/**',
@@ -67,7 +67,7 @@ gulp.task('copy', function() {
             '!node_modules/font-awesome/*.md',
             '!node_modules/font-awesome/*.json'
         ])
-        .pipe(gulp.dest('vendor/font-awesome'))
+        .pipe(gulp.dest('font-awesome'))
 })
 
 // Run everything
